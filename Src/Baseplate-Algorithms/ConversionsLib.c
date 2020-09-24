a4ab7a760fcf1d8a475f7c15c2a7c85bdd5ae31b
HypeByte <69880284+HypeByte@users.noreply.github.com>
2020-09-23 18:02:31 -0700

    String conversions

--- Src/Baseplate-Algorithms/ConversionsLib.c
+++ Src/Baseplate-Algorithms/ConversionsLib.c
@@ -1,306 +1,208 @@
 #include<string.h>
+
 #include<stdbool.h>
+
 #include<stdlib.h>
+
 #include<stdio.h>
-
-
 
 
 //Made by Hype Byte
 
-
-
-
 //Resources
 int power(int input, int exponent) {
 
-int product = 1;
+  int product = 1;
 
-for(int i = 0; i < exponent; i++) {
+  for (int i = 0; i < exponent; i++) {
 
-product*=input;
-}
+    product *= input;
+  }
 
-return product;
-
-
+  return product;
 
 }
-
-
 
 //Parse functions
 
 int parseInt(char * number) { //Convers strings to ints, for example "123" --> 123, "-321" --> -321
 
-    int num = 0;
+  int num = 0;
 
-    int i = number[0] == '-' ? 1 : 0;
+  int i = number[0] == '-' ? 1 : 0;
 
-    for( ; i < strlen(number) ; i++) {
-         
-          num+= (number[i] - '0') * power(10,strlen(number) - i - 1);
-    }
+  for (; i < strlen(number); i++) {
 
-num = number[0] == '-' ? -num : num;
+    num += (number[i] - '0') * power(10, strlen(number) - i - 1);
+  }
 
-return num;
+  num = number[0] == '-' ? -num : num;
+
+  return num;
 
 }
-
 
 bool parseBool(char flag) { //converts char into boolean, for example '1' --> true, '0' --> false
 
-bool result = flag == '1' ? true : flag == '0' ? false : false;
-return result;
+  bool result = flag == '1' ? true : flag == '0' ? false : false;
+  return result;
 
 }
 
-double parseDecimal(char *deci) { // converts char * into a decimal, for example "123.123123" ---> double 123.123123
-    int whole = 0;
-    double decimal = .0;
-    int n = 0;
-    double result;
+double parseDecimal(char * deci) { // converts char * into a decimal, for example "123.123123" ---> double 123.123123
+  int whole = 0;
+  double decimal = .0;
+  int n = 0;
+  double result;
 
-    while(deci[n] != '.') {
-        n++;
-    }
+  while (deci[n] != '.') {
+    n++;
+  }
 
-   char * whs= malloc(n);
+  char * whs = malloc(n);
 
-  for(int q = 0; q < n; q++) {
-      whs[q] = deci[q];
-      
+  for (int q = 0; q < n; q++) {
+    whs[q] = deci[q];
+
   }
 
   whole = parseInt(whs);
 
- for(int z = n + 1; z < ((int)strlen(deci)); z++) {
-     decimal += (double) (deci[z] - '0') * (double) (1/(double)(power(10,z-n)));
- }
+  for (int z = n + 1; z < ((int) strlen(deci)); z++) {
+    decimal += (double)(deci[z] - '0') * (double)(1 / (double)(power(10, z - n)));
+  }
 
-    return (double)whole + decimal;
-
-  
-
+  return (double) whole + decimal;
 
 }
-
 
 void parseIntArr(int * arr, int length, char * S_arr, char sepC) {
 
+  int pos = 0;
+  char * bank;
 
-int pos = 0;
-char * bank;
+  for (int i = 0; i < length; i++) {
 
+    for (int vector = pos; vector < (int) strlen(S_arr) + 1; vector++) {
 
-for(int i = 0; i < length; i++) {
-    
-    
-    for(int vector = pos; vector < (int) strlen(S_arr) + 1; vector++) {
-        
-        if(S_arr[vector] == sepC || vector == (int) strlen(S_arr)) {
-            
-            bank = malloc(vector - pos);
-            
-            for(int n = pos, j = 0; n < vector; n++, j++) {
-                
-                bank[j] = S_arr[n];
-                
-            }
-            
-            arr[i] = parseInt(bank);
-            free(bank);
-            pos = vector + 1;
-            break;
-            
-            
-            
+      if (S_arr[vector] == sepC || vector == (int) strlen(S_arr)) {
+
+        bank = malloc(vector - pos);
+
+        for (int n = pos, j = 0; n < vector; n++, j++) {
+
+          bank[j] = S_arr[n];
+
         }
-        
-        
-    }    
-    
-    
-    
-    
+
+        arr[i] = parseInt(bank);
+        free(bank);
+        pos = vector + 1;
+        break;
+
+      }
+
+    }
+
+  }
+
 }
-
-
-
-    
-    
-    
-    
-}
-
-
-
 
 void parseDeciArr(double * arr, int length, char * S_arr, char sepC) {
 
+  int pos = 0;
+  char * bank;
 
-int pos = 0;
-char * bank;
+  for (int i = 0; i < length; i++) {
 
+    for (int vector = pos; vector < (int) strlen(S_arr) + 1; vector++) {
 
-for(int i = 0; i < length; i++) {
-    
-    
-    for(int vector = pos; vector < (int) strlen(S_arr) + 1; vector++) {
-        
-        if(S_arr[vector] == sepC || vector == (int) strlen(S_arr)) {
-            
-            bank = malloc(vector - pos);
-            
-            for(int n = pos, j = 0; n < vector; n++, j++) {
-                
-                bank[j] = S_arr[n];
-                
-            }
-            arr[i] = parseDecimal("1.23");
-            free(bank);
-            pos = vector + 1;
-            break;
-            
-            
-            
+      if (S_arr[vector] == sepC || vector == (int) strlen(S_arr)) {
+
+        bank = malloc(vector - pos);
+
+        for (int n = pos, j = 0; n < vector; n++, j++) {
+
+          bank[j] = S_arr[n];
+
         }
-        
-        
-    }    
-    
-    
-    
-    
+        arr[i] = parseDecimal("1.23");
+        free(bank);
+        pos = vector + 1;
+        break;
+
+      }
+
+    }
+
+  }
+
 }
-
-
-
-    
-    
-    
-    
-}
-
-
-
-
-
 
 void parseStringArr(char * arr[], int length, char * S_arr, char sepC) {
 
+  int pos = 0;
 
-int pos = 0;
+  for (int i = 0; i < length; i++) {
 
+    for (int vector = pos; vector < (int) strlen(S_arr) + 1; vector++) {
 
+      if (S_arr[vector] == sepC || vector == (int) strlen(S_arr)) {
 
-for(int i = 0; i < length; i++) {
-    
-    
-    for(int vector = pos; vector < (int) strlen(S_arr) + 1; vector++) {
-        
-        if(S_arr[vector] == sepC || vector == (int) strlen(S_arr)) {
-            
-            arr[i] = malloc(vector - pos);
+        arr[i] = malloc(vector - pos);
 
-            
-            for(int n = pos, j = 0; n < vector; n++, j++) {
-                
-                arr[i][j] = S_arr[n];
-                
-            }
-            
-            pos = vector + 1;
-            break;
-            
-            
-            
+        for (int n = pos, j = 0; n < vector; n++, j++) {
+
+          arr[i][j] = S_arr[n];
+
         }
-        
-        
-    }    
-    
-    
-    
-    
+
+        pos = vector + 1;
+        break;
+
+      }
+
+    }
+
+  }
+
 }
-
-
-
-    
-    
-    
-    
-}
-
-
-
-
-
-
 
 void parseCharArr(char * arr, int length, char * S_arr) {
 
+  for (int i = 0; i < (int) strlen(S_arr); i += 2) {
 
+    arr[i / 2] = S_arr[i];
 
-    for(int i = 0; i < (int) strlen(S_arr); i+=2) {
+  }
 
-        arr[i/2] = S_arr[i];
-
-
-    }
-
-
-    
-
-
-      
 }
-
 
 void parseBoolArr(bool * arr, char * S_arr) {
 
+  for (int i = 0; i < (int) strlen(S_arr); i += 2) {
 
+    arr[i / 2] = S_arr[i] == '1' ? true : false;
 
-    for(int i = 0; i < (int) strlen(S_arr); i+=2) {
+  }
 
-        arr[i/2] = S_arr[i] == '1' ? true : false;
-
-
-    }
-
-
-    
-
-
-      
 }
-
-
-
-
 
 long int parseLongInt(char number[]) { //Convers strings to ints, for example "123" --> 123, "-321" --> -321
 
-    long int num = 0;
+  long int num = 0;
 
-    int i = number[0] == '-' ? 1 : 0;
+  int i = number[0] == '-' ? 1 : 0;
 
-    for( ; i < strlen(number) ; i++) {
-         
-          num+= (long int)(number[i] - '0') * (long int)power(10,strlen(number) - i - 1);
-    }
+  for (; i < strlen(number); i++) {
 
-num = number[0] == '-' ? -num : num;
+    num += (long int)(number[i] - '0') * (long int) power(10, strlen(number) - i - 1);
+  }
 
-return num;
+  num = number[0] == '-' ? -num : num;
+
+  return num;
 
 }
 
-
-
-
-
-
-
+© 2020 GitHub, Inc.
