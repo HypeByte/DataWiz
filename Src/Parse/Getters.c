50cc86ab1c4e9a89ed67e02c63883cc632df5129
HypeByte <69880284+HypeByte@users.noreply.github.com>
2020-09-23 18:04:38 -0700

    DataWiz Getter Functions

--- Src/Parse/Getters.c
+++ Src/Parse/Getters.c
@@ -1,277 +1,193 @@
-#include<stdio.h>
-#include "../Baseplate-Algorithms/ConversionsLib.h"
+  #include<stdio.h>
 
+  #include "../Baseplate-Algorithms/ConversionsLib.h"
 
+  int getInteger(char * ElemName, FILE * BankScript) {
+    rewind(BankScript);
+    char LineSnap[100];
+    char valSnap[32];
+    char * nameSnap = malloc(strlen(ElemName));
 
-int getInteger(char * ElemName, FILE * BankScript) {
-rewind(BankScript);
-char LineSnap[100];
-char valSnap[32];
-char * nameSnap = malloc( strlen(ElemName) );
+    while (fgets(LineSnap, 100, BankScript) != NULL) {
 
-    while(fgets(LineSnap,100,BankScript) != NULL ) {
+      if (LineSnap[0] == 'i') {
 
-        if(LineSnap[0] == 'i') {
+        for (int bG = 2; bG < (int) strlen(ElemName) + 2; bG++) {
 
-
-        for(int bG = 2; bG < (int) strlen(ElemName) +  2; bG++) {
-
-            nameSnap[bG - 2] = LineSnap[bG];
-            
+          nameSnap[bG - 2] = LineSnap[bG];
 
         }
-        
 
-        if(strcmp(nameSnap,ElemName) == 0){
+        if (strcmp(nameSnap, ElemName) == 0) {
 
-      
-          for(int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++){
-        
-              valSnap[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
-                
-        }
-          
-       
+          for (int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++) {
 
+            valSnap[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
+
+          }
 
         }
-        
+
         return parseInt(valSnap);
-       break;
-        
+        break;
 
-
-
-
+      }
     }
-}
 
-}
+  }
 
+  double getDecimal(char * ElemName, FILE * BankScript) {
+    rewind(BankScript);
+    char LineSnap[64];
+    char valSnap[32];
+    char * nameSnap = malloc(strlen(ElemName));
 
+    while (fgets(LineSnap, 32, BankScript) != NULL) {
 
+      if (LineSnap[0] == 'd') {
 
+        for (int bG = 2; bG < (int) strlen(ElemName) + 2; bG++) {
 
-
-
-
-double getDecimal(char * ElemName, FILE * BankScript) {
-rewind(BankScript);
-char LineSnap[64];
-char valSnap[32];
-char * nameSnap = malloc( strlen(ElemName) );
-
-    while(fgets(LineSnap,32,BankScript) != NULL ) {
-
-        if(LineSnap[0] == 'd') {
-
-
-        for(int bG = 2; bG < (int) strlen(ElemName) +  2; bG++) {
-
-            nameSnap[bG - 2] = LineSnap[bG];
-            
+          nameSnap[bG - 2] = LineSnap[bG];
 
         }
 
-        if(strcmp(nameSnap,ElemName) == 0){
+        if (strcmp(nameSnap, ElemName) == 0) {
 
-            for(int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++){
-                
-                valSnap[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
+          for (int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++) {
 
-        }
+            valSnap[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
+
+          }
 
         }
 
         return parseDecimal(valSnap);
-       break;
-        
+        break;
 
-
-
-
+      }
     }
-}
 
-}
+  }
 
+  char getChar(char * ElemName, FILE * BankScript) {
+    rewind(BankScript);
+    char LineSnap[64];
+    char * nameSnap = malloc(strlen(ElemName));
 
+    while (fgets(LineSnap, 64, BankScript) != NULL) {
 
+      if (LineSnap[0] == 'c') {
 
-char getChar(char *ElemName, FILE * BankScript) {
-rewind(BankScript);
-char LineSnap[64];
-char * nameSnap = malloc( strlen(ElemName) );
+        for (int bG = 2; bG < (int) strlen(ElemName) + 2; bG++) {
 
-while( fgets(LineSnap,64,BankScript) != NULL) {
-
-    if(LineSnap[0] == 'c') {
-    
-      for(int bG = 2; bG < (int) strlen(ElemName) +  2; bG++) {
-
-            nameSnap[bG - 2] = LineSnap[bG];
-            
+          nameSnap[bG - 2] = LineSnap[bG];
 
         }
 
-        if(strcmp(nameSnap,ElemName) == 0) {
+        if (strcmp(nameSnap, ElemName) == 0) {
 
-            return LineSnap[3 + strlen(ElemName)];
-            break;
+          return LineSnap[3 + strlen(ElemName)];
+          break;
 
         }
 
-
+      }
 
     }
 
-}
+  }
 
+  void LoadString(char * ElemName, FILE * BankScript, char * target) {
+    rewind(BankScript);
+    char LineSnap[64];
+    char * nameSnap = malloc(strlen(ElemName));
 
+    while (fgets(LineSnap, 64, BankScript) != NULL) {
 
+      if (LineSnap[0] == 's') {
 
+        for (int bG = 2; bG < (int) strlen(ElemName) + 2; bG++) {
 
-
-}
-
-
-
-
-void LoadString(char * ElemName, FILE * BankScript, char * target) {
-rewind(BankScript);
-char LineSnap[64];
-char * nameSnap = malloc( strlen(ElemName) );
-
-    while(fgets(LineSnap,64,BankScript) != NULL ) {
-       
-        if(LineSnap[0] == 's') {
-           
-
-        for(int bG = 2; bG < (int) strlen(ElemName) +  2; bG++) {
-
-            nameSnap[bG - 2] = LineSnap[bG];
-            
+          nameSnap[bG - 2] = LineSnap[bG];
 
         }
 
-        if(strcmp(nameSnap,ElemName) == 0){
+        if (strcmp(nameSnap, ElemName) == 0) {
 
-            for(int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++){
-                
-                target[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
-                
+          for (int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++) {
+
+            target[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
+
+          }
 
         }
 
-        }
+        break;
 
-        
-
-
-
-
-       break;
-        
-
-
-
-
+      }
     }
-}
 
-}
+  }
 
+  char getBool(char * ElemName, FILE * BankScript) {
+    rewind(BankScript);
+    char LineSnap[64];
+    char * nameSnap = malloc(strlen(ElemName));
 
+    while (fgets(LineSnap, 64, BankScript) != NULL) {
 
+      if (LineSnap[0] == 'b') {
 
-char getBool(char *ElemName, FILE * BankScript) {
-rewind(BankScript);
-char LineSnap[64];
-char * nameSnap = malloc( strlen(ElemName) );
+        for (int bG = 2; bG < (int) strlen(ElemName) + 2; bG++) {
 
-while( fgets(LineSnap,64,BankScript) != NULL) {
-
-    if(LineSnap[0] == 'b') {
-    
-      for(int bG = 2; bG < (int) strlen(ElemName) +  2; bG++) {
-
-            nameSnap[bG - 2] = LineSnap[bG];
-            
+          nameSnap[bG - 2] = LineSnap[bG];
 
         }
 
-        if(strcmp(nameSnap,ElemName) == 0) {
+        if (strcmp(nameSnap, ElemName) == 0) {
 
-            return LineSnap[3 + strlen(ElemName)] == '1' ? true : false;
-            break;
+          return LineSnap[3 + strlen(ElemName)] == '1' ? true : false;
+          break;
 
         }
 
-
+      }
 
     }
 
-}
+  }
 
+  long int getLongInteger(char * ElemName, FILE * BankScript) {
+    rewind(BankScript);
+    char LineSnap[100];
+    char valSnap[32];
+    char * nameSnap = malloc(strlen(ElemName));
 
+    while (fgets(LineSnap, 100, BankScript) != NULL) {
 
+      if (LineSnap[0] == 'l') {
 
+        for (int bG = 2; bG < (int) strlen(ElemName) + 2; bG++) {
 
-
-}
-
-
-
-long int getLongInteger(char * ElemName, FILE * BankScript) {
-rewind(BankScript);
-char LineSnap[100];
-char valSnap[32];
-char * nameSnap = malloc( strlen(ElemName) );
-
-    while(fgets(LineSnap,100,BankScript) != NULL ) {
-
-        if(LineSnap[0] == 'l') {
-
-
-        for(int bG = 2; bG < (int) strlen(ElemName) +  2; bG++) {
-
-            nameSnap[bG - 2] = LineSnap[bG];
-            
+          nameSnap[bG - 2] = LineSnap[bG];
 
         }
-        
 
-        if(strcmp(nameSnap,ElemName) == 0){
+        if (strcmp(nameSnap, ElemName) == 0) {
 
-      
-          for(int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++){
-        
-              valSnap[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
-                
-        }
-          
-       
+          for (int vGet = 3 + (int) strlen(ElemName); LineSnap[vGet] != ';'; vGet++) {
 
+            valSnap[vGet - (3 + (int) strlen(ElemName))] = LineSnap[vGet];
+
+          }
 
         }
-        
+
         return parseLongInt(valSnap);
-       break;
-        
+        break;
 
-
-
-
+      }
     }
-}
 
-}
-
-
-
-
-
-
-
-
-
+  }
