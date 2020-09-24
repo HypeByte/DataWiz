aaf5aee6942b31dc95d7aadf1cf8e7fb12f1ac58
HypeByte <69880284+HypeByte@users.noreply.github.com>
2020-09-23 18:05:12 -0700

    Getters Module

--- Src/Parse/Getters.h
+++ Src/Parse/Getters.h
@@ -1,10 +1,6 @@
 #include<stdio.h>
 #include"../Baseplate-Algorithms/ConversionsLib.h"
 
-
-
-
-
 int getInteger(char * ElemName, FILE * BankScript);
 double getDecimal(char * ElemName, FILE * BankScript);
 void LoadString(char * ElemName, FILE * BankScript, char * target);
